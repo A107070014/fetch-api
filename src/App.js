110c9df257508af79035234bb7d9fc1951c0f3ca
李欣玫 <fesu308@gmail.com>
2022-11-20 00:22:08 +0800

    [modify]infinite scroll

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import React from "react";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import styles from './App.module.scss';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
+import { FaDog, FaCat, FaImage } from 'react-icons/fa';
 
 function App() {
   const [allData, setAllData] = useState([]);
   const [data, setData] = useState([]);
   const [number, setNumber] = useState(10);
   const [loading, setLoading] = useState(true);
+  const targetRef = useRef(null);
 
   useEffect(() => {
     const _fetchData = async () => {
@@ -28,8 +27,6 @@ function App() {
     _fetchData();
   }, [])
 
-  console.log(allData);
-
   useEffect(() => {
     function _loadingNewData() {
       console.log("_loadingNewData()");
@@ -40,24 +37,45 @@ function App() {
     }
     _loadingNewData();
   }, [number, loading]) // 跟上面的useEffect會同時執行，因此第一次allData為[]，需在dependencies加上loading，再執行一次此useEffect
+  const options = {
+    threshold: 1, //設定目標元素的可見度達到多少比例時，觸發 callback 函式
+  }
 
+  const callback = (entries, observer) => {
+    // entries 能拿到所有目標元素進出(intersect)變化的資訊
+    entries.forEach(entry => {
+      console.log(entry.isIntersecting);
+      if (entry.isIntersecting) {
+        //  只在目標元素進入 viewport 時執行這裡的工作
+        setNumber(preNumber => preNumber + 10);
+        // observer.unobserve(targetRef.current);
+      } else {
+        // 只在目標元素離開 viewport 時執行這裡的工作
+      }
+    })
+  }
 
+  useEffect(() => {
+    const observer = new IntersectionObserver(callback, options);
+
+    // 設定觀察對象：告訴 observer 要觀察哪個目標元素
+    if (targetRef.current) observer.observe(targetRef.current);
+  }, [targetRef, options])
+
+  console.log(allData);
+  console.log(targetRef);
+  console.log(number);
 
   return (
     <div className={styles.block}>
       {loading && <div className={styles.block_row}>loading</div>}
       <div className={styles.block_row}>
         {data.map((item, index) =>
-          <div className={styles.block_col}>
+          <div className={styles.block_col} ref={targetRef}>
             {item.album_file ?
-              <img className={styles.block_img} src={item.album_file} key={index} alt="動物圖片" /> : 
-              <FontAwesomeIcon 
-                className={styles.block_img} 
-                icon={
-                  item.animal_kind == '狗' ? ['fas', 'dog'] : 
-                  item.animal_kind == '貓' ? ['fas', 'cat'] : ['fas', 'paw-simple']
-                } 
-              /> 
+              <img className={styles.block_img} src={item.album_file} key={index} alt="動物圖片" /> :
+              item.animal_kind === '狗' ? <FaDog className={styles.block_img} /> :
+                item.animal_kind === '貓' ? <FaCat className={styles.block_img} /> : <FaImage className={styles.block_img} />
             }
             <div className={styles.block_animal}>
               <div>序號：{index + 1}</div>
@@ -71,7 +89,6 @@ function App() {
           </div>
         )}
       </div>
-      <div className={styles.block_row} onClick={() => setNumber(preNumber => preNumber + 10)}>click</div>
     </div>
   );
 }
